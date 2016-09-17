6a9167a661ac12d9d18aa0633d83050e7dafd077
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2016-09-16 23:17:33 -0300

    Login working fantabulosirijillously.

--- ng/navigation.ctrl.js
+++ ng/navigation.ctrl.js
@@ -3,10 +3,7 @@ angular.module('app')
   var vm = this;
   vm.isLoggedIn = AuthenticationSvc.isLoggedIn();
   vm.currentUser =  AuthenticationSvc.currentUser();
-  console.log("Datos de usuario: ");
-  console.log(vm.currentUser);
   vm.logout = function(){
-    console.log('Logging out...');
     AuthenticationSvc.logout();
     $location.path('/');
   }
